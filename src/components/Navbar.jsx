9949263a7678cc892ea12ff14799abddc8101a5c
peculiar <pecuiheubammadu1st@gmail.com>
2024-09-18 16:03:20 +0100

    Fixed Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,21 +32,21 @@ function Navbar() {
 
                     {/* -------------------------- Search Bar --------------------------------------------------> */}
 
-                <div className='relative hidden sm:flex items-center justify-center gap-3 pr-8 cursor-pointer'>
-                    <LuSearch className='absolute left-3 text-2xl text-gray-500' />
+                <div className='relative hidden sm:flex items-center justify-center gap-3 pr-2 cursor-pointer'>
+                    <LuSearch className='absolute left-1 text-2xl text-gray-500' />
                     <input type="text" placeholder='Search...'
-                        className='py-2 px-10 rounded-xl border-2 border-blue-300 focus:bg-slate-100 focus:outline-sky-500 cursor-pointer' 
+                        className='capitalize py-2 px-7 rounded-xl border-2 border-blue-300 focus:bg-slate-100 focus:outline-sky-500 cursor-pointer' 
                     />
                 </div>
 
                     {/* ------------------------------ Desktop View Menu ------------------------------------------------------------> */}
 
-                <ul className='hidden md:flex items-center gap-5 font-semibold text-[#212a3c] text-lg justify-end'>
-                    <li className='p-3 hover:bg-sky-400 hover:text-white rounded-md hover:font-bold hover:shadow-inner hover:shadow-neutral-400 hover:p-4 transition-all capitalize cursor-pointer'>home</li>
-                    <li className='p-3 hover:bg-sky-400 hover:text-white rounded-md hover:font-bold hover:shadow-inner hover:shadow-neutral-400 hover:p-4 transition-all capitalize cursor-pointer'>about</li>
-                    <li className='p-3 hover:bg-sky-400 hover:text-white rounded-md hover:font-bold hover:shadow-inner hover:shadow-neutral-400 hover:p-4 transition-all capitalize cursor-pointer'>books</li>
-                    <li className='p-3 hover:bg-sky-400 hover:text-white rounded-md hover:font-bold hover:shadow-inner hover:shadow-neutral-400 hover:p-4 transition-all capitalize cursor-pointer'>contact</li>
-                    <li className='p-3 hover:bg-sky-400 hover:text-white rounded-md hover:font-bold hover:shadow-inner hover:shadow-neutral-400 hover:p-4 transition-all capitalize cursor-pointer'>blog</li>
+                <ul className='hidden md:flex items-center gap-5 p-6 font-semibold text-[#212a3c] text-lg justify-end'>
+                    <li className='list-none p-1 border-b-2 border-white hover:border-fuchsia-600 hover:text-fuchsia-600 hover:font-bold transition-all capitalize cursor-pointer'>home</li>
+                    <li className='list-none p-1 border-b-2 border-white hover:border-fuchsia-600 hover:text-fuchsia-600 hover:font-bold transition-all capitalize cursor-pointer'>about</li>
+                    <li className='list-none p-1 border-b-2 border-white hover:border-fuchsia-600 hover:text-fuchsia-600 hover:font-bold transition-all capitalize cursor-pointer'>books</li>
+                    <li className='list-none p-1 border-b-2 border-white hover:border-fuchsia-600 hover:text-fuchsia-600 hover:font-bold transition-all capitalize cursor-pointer'>contact</li>
+                    <li className='list-none p-1 border-b-2 border-white hover:border-fuchsia-600 hover:text-fuchsia-600 hover:font-bold transition-all capitalize cursor-pointer'>blog</li>
                 </ul>
 
                     {/* ---------------------------------- Theme Toggle ---------------------------------------------------------> */}
@@ -65,14 +65,14 @@ function Navbar() {
 
                    {/* ------------------------------ Mobile View Menu ------------------------------------------------------------> */}
 
-                <div className={`absolute md:hidden top-28 right-4 w-80 bg-white rounded-xl flex flex-col items-center gap-6 p-2 font-semibold text-[#212a3c] text-lg transform transition-transform
+                <div className={`absolute md:hidden top-28 right-4 w-80 bg-white rounded-xl flex flex-col text-center gap-6 px-32 py-6 font-semibold text-[#212a3c] text-lg transform transition-transform
                     ${isMenuOpen ? "block" : "hidden"}`}>
                     {/* style={{transition: " transform 0.5s ease, opacity 0.5s ease"}}> */}
-                    <li className='list-none w-full text-center p-4 hover:bg-sky-400 rounded-xl hover:text-white hover:font-bold hover:shadow-inner hover:shadow-neutral-400 hover:p-5 transition-all cursor-pointer capitalize' onClick = {toggleMenu}>home</li>
-                    <li className='list-none w-full text-center p-4 hover:bg-sky-400 rounded-xl hover:text-white hover:font-bold hover:shadow-inner hover:shadow-neutral-400 hover:p-5 transition-all cursor-pointer capitalize' onClick = {toggleMenu}>about</li>
-                    <li className='list-none w-full text-center p-4 hover:bg-sky-400 rounded-xl hover:text-white hover:font-bold hover:shadow-inner hover:shadow-neutral-400 hover:p-5 transition-all cursor-pointer capitalize' onClick = {toggleMenu}>books</li>
-                    <li className='list-none w-full text-center p-4 hover:bg-sky-400 rounded-xl hover:text-white hover:font-bold hover:shadow-inner hover:shadow-neutral-400 hover:p-5 transition-all cursor-pointer capitalize' onClick = {toggleMenu}>contact</li>
-                    <li className='list-none w-full text-center p-4 hover:bg-sky-400 rounded-xl hover:text-white hover:font-bold hover:shadow-inner hover:shadow-neutral-400 hover:p-5 transition-all cursor-pointer capitalize' onClick = {toggleMenu}>blog</li>
+                    <li className='list-none p-1 border-b-2 border-white hover:border-fuchsia-600 hover:text-fuchsia-600 hover:font-bold transition-all cursor-pointer capitalize' onClick = {toggleMenu}>home</li>
+                    <li className='list-none p-1 border-b-2 border-white hover:border-fuchsia-600 hover:text-fuchsia-600 hover:font-bold transition-all cursor-pointer capitalize' onClick = {toggleMenu}>about</li>
+                    <li className='list-none p-1 border-b-2 border-white hover:border-fuchsia-600 hover:text-fuchsia-600 hover:font-bold transition-all cursor-pointer capitalize' onClick = {toggleMenu}>books</li>
+                    <li className='list-none p-1 border-b-2 border-white hover:border-fuchsia-600 w-fit hover:text-fuchsia-600 hover:font-bold transition-all cursor-pointer capitalize' onClick = {toggleMenu}>contact</li>
+                    <li className='list-none p-1 border-b-2 border-white hover:border-fuchsia-600 hover:text-fuchsia-600 hover:font-bold transition-all cursor-pointer capitalize' onClick = {toggleMenu}>blog</li>
                 </div>
 
             </nav>
---
+++ src/components/Theme.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Theme = () => {
+  return (
+    <div>Theme</div>
+  )
+}
+
+export default Theme
+
